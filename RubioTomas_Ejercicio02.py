9a296c298b3dac104607b09e2efe1f79b1edeb97
TmsRC <t.rubio28@uniandes.edu.co>
2020-01-24 15:43:03 +0000

    Segunda parte completa. Arreglos primera parte

--- RubioTomas_Ejercicio02.py
+++ RubioTomas_Ejercicio02.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy.linalg as la
 
-datosFull = np.loadtxt('../ejercicios/02/numeros_20.txt')
+datosFull = np.loadtxt('numeros_20.txt')
 x = datosFull[:,0]
 y = datosFull[:,1]
+
+# Primera Parte ---------------------------------------------------------------------------------------------------------
 
 def fitCoef(x,y,M):
     S = np.power(np.transpose(np.repeat(x[None],M+1,axis=0)),np.linspace(0,M,M+1))
@@ -25,36 +27,89 @@ E_rms = [[],[]]
 for i in range(0,10):
     
     x_test = np.linspace(0,1)
-    coefs = fitCoef(x[:9],y[:9],i)
+    coefs = fitCoef(x[:10],y[:10],i)
     y_fit = polyGen(coefs,x_test) #Si era el otro fit, cambiar esto.
     
-    E_rms[0].append(np.sqrt(chiSquare(polyGen(coefs,x[:9]),y[:9]))/10)
+    E_rms[0].append(np.sqrt(chiSquare(polyGen(coefs,x[:10]),y[:10]))/10)
     E_rms[1].append(np.sqrt(chiSquare(polyGen(coefs,x[10:]),y[10:]))/10)
     
     if i in Ms:
         a = axes[Ms.index(i)]
         a.scatter(x[10:],y[10:],facecolors='none',edgecolors='b')
-        a.scatter(x[:9],y[:9],c='grey')
+        a.scatter(x[:10],y[:10],c='grey')
         a.plot(x_test,y_fit,c='r')
         a.set_title("M = "+str(i))
         a.set_ylim(0,2)
         if i == 9:
-            a.set_ylim(-200,300)
+            a.set_ylim(-2000,2000)
     
 fig.tight_layout()
+fig.savefig('PrimeraParte_Figura1.png')
 
-fig,ax1 = plt.subplots()
 Ms = range(0,10)
 
-ax1.plot(Ms,E_rms[0],c='r')
-ax1.scatter(Ms,E_rms[0],c='r')
-ax1.set_ylabel('Error datos entrenamiento')
-ax1.set_xlabel('M')
+fig = plt.figure()
+plt.plot(Ms,E_rms[0],c='r',label='Datos entrenamiento')
+plt.scatter(Ms,E_rms[0],c='r')
+plt.plot(Ms,E_rms[1],c='b',label='Datos prueba')
+plt.scatter(Ms,E_rms[1],c='b')
+plt.yscale('log')
+plt.legend()
+plt.savefig('PrimeraParte_Figura2.png')
 
-ax2 = ax1.twinx()
-ax2.plot(Ms,E_rms[1],c='b')
-ax2.scatter(Ms,E_rms[1],c='b')
-ax2.set_ylim(0,200)
-ax2.set_ylabel('Error datos prueba')
 
-fig.legend(['Datos entrenamiento',None,'Datos prueba',None])
+# Segunda Parte ---------------------------------------------------------------------------------------------------------------
+
+class PolyFit():
+    def __init__(self,degree=0):
+        self.M = degree+1
+        self.betas = np.array([])
+    def fit(self,x,y):
+        S = np.power(np.transpose(np.repeat(x[None],self.M,axis=0)),np.linspace(0,self.M-1,self.M))
+        Sinv = la.pinv(S)
+        self.betas = np.dot(Sinv,y)
+    def predict(self,x):
+        powers = np.power(np.transpose(np.repeat(x[None],self.M,axis=0)),np.linspace(0,self.M-1,self.M))
+        return np.sum(np.multiply(powers,self.betas),axis=1)
+    def score(self,x,y):
+        chi = np.sum(np.power(np.subtract(self.predict(x),y),2))
+        return np.sqrt(chi/len(y))
+
+fig,axii = plt.subplots(2,2)
+
+axes = list(axii[0])+list(axii[1])
+Ms = [0,1,3,9]
+E_rms = [[],[]]
+for i in range(0,10):
+    hola = PolyFit(i)
+    x_test = np.linspace(0,1)
+    coefs = hola.fit(x[:10],y[:10])
+    y_fit = hola.predict(x_test)
+    
+    E_rms[0].append(hola.score(x[:10],y[:10]))
+    E_rms[1].append(hola.score(x[10:],y[10:]))
+    
+    if i in Ms:
+        a = axes[Ms.index(i)]
+        a.scatter(x[10:],y[10:],facecolors='none',edgecolors='b')
+        a.scatter(x[:10],y[:10],c='grey')
+        a.plot(x_test,y_fit,c='r')
+        a.set_title("M = "+str(i))
+        a.set_ylim(0,2)
+        if i == 9:
+            a.set_ylim(-2000,2000)
+    
+fig.tight_layout()
+fig.savefig('SegundaParte_Figura1.png')
+
+
+Ms = range(0,10)
+
+fig = plt.figure()
+plt.plot(Ms,E_rms[0],c='r',label='Datos entrenamiento')
+plt.scatter(Ms,E_rms[0],c='r')
+plt.plot(Ms,E_rms[1],c='b',label='Datos prueba')
+plt.scatter(Ms,E_rms[1],c='b')
+plt.yscale('log')
+plt.legend()
+plt.savefig('SegundaParte_Figura2.png')
